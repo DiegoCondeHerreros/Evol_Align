61fb8282582bac6e183c5f844986148822fd0f4f
George Hannah <80538951+Ge0rgeHannah@users.noreply.github.com>
2026-07-08 16:01:20 +0200

    test: notification for user input

--- chunking_experiment.py
+++ chunking_experiment.py
@@ -277,6 +277,20 @@ class AlignmentEvaluation():
 
     def __init__(self, experiment):
         self.exp = experiment
+        self.set_judge_model()
+
+    def set_judge_model(self):
+        # NOTE: Whilst LLM-as-a-judge has shown potential, there is
+        # always the possibility of a simpler binary classification model
+        # is there any way where we could use the a small human evaluation
+        # session to fine tune an exisiting classifier? Do these even exist?
+        model = LLM(
+            model_family="Gemini",
+            model="gemini-3.5-flash",
+            params={"temperature": 0.0, "seed": 7264},
+            context=None
+        )
+        self.judge = model
 
 
 def run_experiment():
